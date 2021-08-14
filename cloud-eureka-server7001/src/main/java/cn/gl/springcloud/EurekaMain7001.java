2d1de4c483a498a779a0de18c406d03a73f76d6f
gl <1623714242@qq.com>
2021-08-14 17:04:11 +0800

    服务提供者与消费者注册入eureka

--- cloud-eureka-server7001/src/main/java/cn/gl/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/cn/gl/springcloud/EurekaMain7001.java
@@ -2,16 +2,15 @@ package cn.gl.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author gl
- * @date 2021/6/14 15:07
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7001.class);
     }
 }
